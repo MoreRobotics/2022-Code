3b91dd5323c348c8cef53923e5d75df242dc4aee
MORE Robotics <robotics@stm.local>
2022-03-01 03:38:35 -0600

    added climber stuff, didn't feel like merging

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -16,35 +16,58 @@ import frc.robot.Constants;
 
 public class Climber extends SubsystemBase {
   /** Creates a new Climber. */
-  WPI_TalonFX climberLeft, climberRight;
+  WPI_TalonFX climberLeft, climberRight, climberMid;
   XboxController operatorController;
 
   public Climber() {
     climberLeft = new WPI_TalonFX(Constants.CLIMBER_LEFT_MOTOR_ID);
     climberRight = new WPI_TalonFX(Constants.CLIMBER_RIGHT_MOTOR_ID);
+    climberMid = new WPI_TalonFX(Constants.CLIMBER_MID_MOTOR_ID);
     operatorController = new XboxController(Constants.OPERATOR_CONTROLLER_PORT);
 
     //resets encoders
-    // climberLeft.configFactoryDefault();
-    // climberRight.configFactoryDefault();
+    climberLeft.configFactoryDefault();
+    climberRight.configFactoryDefault();
+    climberMid.configFactoryDefault();
 
     //sets the right climber motor to follow the left one
-    climberRight.follow(climberRight);
+    climberRight.follow(climberLeft);
 
     //sets maximum and minimum power limits for the motors
-    // climberLeft.configNominalOutputForward(0, Constants.kTimeoutMs);
-    // climberLeft.configNominalOutputReverse(0, Constants.kTimeoutMs);
-    // climberLeft.configPeakOutputForward(1, Constants.kTimeoutMs);
-    // climberLeft.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+    climberLeft.configNominalOutputReverse(0, Constants.kTimeoutMs);
+    climberLeft.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+    climberLeft.configPeakOutputForward(1, Constants.kTimeoutMs);
+    climberLeft.configNominalOutputForward(0, Constants.kTimeoutMs);
 
+    climberMid.configNominalOutputReverse(0, Constants.kTimeoutMs);
+    climberMid.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+    climberMid.configPeakOutputForward(1, Constants.kTimeoutMs);
+    climberMid.configNominalOutputForward(0, Constants.kTimeoutMs);
+
+    // climberLeft.config_kF(Constants.CLIMBER_SLOT_INDEX_ID, Constants.kGains_Climber_Rotation_Speed.kF, Constants.kTimeoutMs);
+    // climberLeft.config_kP(Constants.CLIMBER_SLOT_INDEX_ID, Constants.kGains_Climber_Rotation_Speed.kP, Constants.kTimeoutMs);
+    // climberLeft.config_kI(Constants.CLIMBER_SLOT_INDEX_ID, Constants.kGains_Climber_Rotation_Speed.kI, Constants.kTimeoutMs);
+    // climberLeft.config_kD(Constants.CLIMBER_SLOT_INDEX_ID, Constants.kGains_Climber_Rotation_Speed.kD, Constants.kTimeoutMs);
+    
   }
 
-  public void stopLeft() {
-    //stop motor
-    climberLeft.set(ControlMode.PercentOutput, 0);
+  public void rotateBack() {
+    //rotate the right and left climber motors to meet specified degrees
+    climberLeft.set(ControlMode.PercentOutput, -0.1);
   }
 
+  public void rotateForward() {
+    //rotate the right and left climber motors to meet specified degrees
+    climberLeft.set(ControlMode.PercentOutput, 0.1);
+  }
 
+  public void extendMiddle() {
+    climberMid.set(ControlMode.PercentOutput, 0.1);
+  }
+
+  public void retractMiddle() {
+    climberMid.set(ControlMode.PercentOutput, -0.1);
+  }
 
   @Override
   public void periodic() {
